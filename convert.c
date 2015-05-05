7fa5dad40bb9f2c91660249670ef123794e8753d
jstaahl <jstaahl@umail.ucsb.edu>
2015-05-05 00:44:33 -0700

    Modified convert.c to encode MIPS string instructions to integers

--- convert.c
+++ convert.c
@@ -1,11 +1,112 @@
-#include "stdio.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    FILE *f = fopen("testinput.txt", "r");
-    int instruction;
-    int line = 0;
-    while(fscanf(f, "%d[^\n]",&instruction) == 1) {
-      printf("%d\n", instruction);
-      line++;
+int encodeRFormat(int op, int func);
+int encodeIFormat(int op);
+int encodeJFormat(int op);
+int regToInt(char *reg);
+
+int main(int argc, char *argv[]) {
+    FILE *f = fopen(argv[1], "r");
+    char *line = NULL;
+    size_t len = 0;
+        
+    char *token;
+    while (getline(&line, &len, f) != -1) {
+      token = strtok(line, " ");
+
+      if (!strcmp(token, "addi")) {
+	printf("%d\n", encodeIFormat(0b110001));
+      } else if (!strcmp(token, "add")) {
+	printf("%d\n", encodeRFormat(0b100000, 0b100000));
+      } else if (!strcmp(token, "and")) {
+	printf("%d\n", encodeRFormat(0b100000, 0b100100));
+      } else if (!strcmp(token, "sub")) {
+	printf("%d\n", encodeRFormat(0b100000, 0b101000));
+      } else if (!strcmp(token, "sgt")) {
+	printf("%d\n", encodeRFormat(0b100000, 0b110000));
+      } else if (!strcmp(token, "lw")) {
+	printf("%d\n", encodeIFormat(0b010001));
+      } else if (!strcmp(token, "sw")) {
+	printf("%d\n", encodeIFormat(0b010010));
+      } else if (!strcmp(token, "beq")) {
+	printf("%d\n", encodeIFormat(0b001010));
+      } else if (!strcmp(token, "jr")) {
+	printf("%d\n", encodeRFormat(0b100101, 0));
+      } else if (!strcmp(token, "jal")) {
+
+      }
     }
+}
+
+int encodeRFormat(int op, int func) {
+  int rs, rt, rd, shamt;
+  char *token;
+  int instruction;
+  rs = rt = rd = shamt = 0;
+
+  rd = regToInt(strtok(NULL, ", "));
+  token = strtok(NULL, ", ");
+  if (token != NULL) {
+    rs = regToInt(token);
+    rt = regToInt(strtok(NULL, ", "));
+  } else {
+    rs = rd;
+    rd = 0;
+  }
+
+  //format and concatenate
+  instruction = op << 26;
+  instruction += (rs & 31) << 21;
+  instruction += (rt & 31) << 16;
+  instruction += (rd & 31) << 11;
+  instruction += (shamt & 0x1f) << 6;
+  instruction += (func & 0x3f);
+
+  return instruction;
+}
+
+int encodeIFormat(int op) {
+  int rs, rt, imm;
+  int instruction;
+  rs = rt = imm = 0;
+
+  char *token = strtok(NULL, ", ");
+  rt = regToInt(token);
+  token = strtok(NULL, ", ");
+  rs = regToInt(token);
+  token = strtok(NULL, ", ");
+  imm = atoi(token);
+
+  /*
+  rt = regToInt(strtok(NULL, ", "));
+  rs = regToInt(strtok(NULL, ", "));
+  imm = atoi(strtok(NULL, ", "));
+  */
+
+  //format and concatenate
+  instruction = op << 26;
+  instruction += (rs & 31) << 21;
+  instruction += (rt & 31) << 16;
+  instruction += (imm & 0xffff);
+
+  return instruction;
+}
+
+int encodeJFormat(int op) {
+  int imm;
+  int instruction;
+  imm = 0;
+
+  imm = atoi(strtok(NULL, ", "));
+
+  instruction = op << 26;
+  instruction += (imm & 0x3ffffff);
+  
+  return -1;
+}
+
+int regToInt(char *reg) {
+  return atoi(&(reg[1]));
 }
